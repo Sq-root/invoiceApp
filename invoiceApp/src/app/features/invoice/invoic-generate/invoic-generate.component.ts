c670747e236ba5e9ea6e4a75476d65f93284e36b
unknown <gauravdavda34@gmail.com>
2024-02-18 19:39:29 +0530

    Invoice Generation UI

--- invoiceApp/src/app/features/invoice/invoic-generate/invoic-generate.component.ts
+++ invoiceApp/src/app/features/invoice/invoic-generate/invoic-generate.component.ts
@@ -20,7 +20,30 @@ export class InvoicGenerateComponent implements OnInit {
       inventoryStatus: 'INSTOCK',
       rating: 5,
     },
-
+    {
+      id: '1001',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+    {
+      id: '1001',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
   ];
 
   //ProductDropdown
