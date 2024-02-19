8c4c1cc685d181f9704d728c4d477c0a9e8c327d
unknown <gauravdavda34@gmail.com>
2024-02-19 08:27:21 +0530

    Invoice Form Binding

--- invoiceApp/src/app/features/invoice/invoic-generate/invoic-generate.component.ts
+++ invoiceApp/src/app/features/invoice/invoic-generate/invoic-generate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-invoic-generate',
@@ -7,6 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoicGenerateComponent implements OnInit {
   value;
+  timeCategories = ['Morning', 'Afternoon', 'Evening', 'Night'];
+  unitList = [
+    {
+      id: 1,
+      item: 'Kg',
+    },
+    {
+      id: 2,
+      item: 'gram',
+    },
+    {
+      id: 3,
+      item: 'Pc',
+    },
+    {
+      id: 4,
+      item: 'Dozen',
+    },
+    {
+      id: 5,
+      item: 'Box',
+    },
+  ];
   products = [
     {
       id: '1000',
@@ -61,30 +85,81 @@ export class InvoicGenerateComponent implements OnInit {
   ];
   selectedCountry: string | undefined;
   selectedUnit;
-  unitList = [
-    {
-      id: 1,
-      item: 'Kg',
-    },
-    {
-      id: 2,
-      item: 'gram',
-    },
-    {
-      id: 3,
-      item: 'Pc',
-    },
-    {
-      id: 4,
-      item: 'Dozen',
-    },
-    {
-      id: 5,
-      item: 'Box',
-    },
-  ];
 
-  constructor() {}
+  //Form
+  invoiceForm: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    //On Load Methods
+    this.getInitalizedForm();
+  }
+
+  // Method : Initalized Invoice Form
+  getInitalizedForm() {
+    this.invoiceForm = this.formBuilder.group({
+      invoiceNo: ['', Validators.required],
+      invoiceDate: ['', Validators.required],
+      invoiceTime: ['', Validators.required],
+      customerDetails: this.formBuilder.group({
+        customerName: [''],
+        customerEmail: [''],
+        customerPhoneNumber: [''],
+        customerMobileNumber: [''],
+        customerAddress1: [''],
+        customerAddress2: [''],
+      }),
+      BusinessDetails: this.formBuilder.group({
+        BusinessName: [''],
+        BusinessEmail: [''],
+        BusinessPhoneNumber: [''],
+        BusinessMobileNumber: [''],
+        BusinessAddress1: [''],
+        BusinessAddress2: [''],
+      }),
+      BillOfproducts: this.formBuilder.array([
+        this.formBuilder.group({
+          productName: ['', Validators.required],
+          quantity: ['', [Validators.required]],
+          unit: ['', Validators.required],
+          rate: ['', Validators.required],
+          amount: ['', Validators.required],
+        }),
+      ]),
+    });
+  }
+
+  // Method: Used to get only product table obj
+  get BillofproductArray() {
+    return this.invoiceForm.get('BillOfproducts') as FormArray;
+  }
+
+  //Method: Add New Product for Invoice
+  addNewProduct() {
+    let newEntity = this.invoiceForm.get('BillOfproducts') as FormArray;
+    newEntity.push(
+      this.formBuilder.group({
+        productName: ['', Validators.required],
+        quantity: ['', Validators.required],
+        unit: ['', Validators.required],
+        rate: ['', Validators.required],
+        amount: ['', Validators.required],
+      })
+    );
+
+    console.log('New ; ', newEntity);
+  }
+
+  //Method: Remove New Product for Invoice
+  removeProduct(index: number) {
+    this.BillofproductArray.removeAt(index);
+  }
+
+  // Method : Generate Bill and Submti to API
+  getInvoiceDetails() {
+    if (this.invoiceForm.valid) {
+      console.log('Invoice Form  :', this.invoiceForm);
+    }
+  }
 }
